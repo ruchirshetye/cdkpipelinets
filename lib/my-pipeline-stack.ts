a95edd4c4505645a4c5151c4355baccee60c55de
Shetye <shetyer@147dda820123.ant.amazon.com>
2022-06-14 12:52:58 +0530

    initial commit4

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -10,7 +10,7 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipelineNewNow',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('ruchirshetye/cdkpipelinets', 'main'),
+        input: CodePipelineSource.gitHub('ruchirshetye/cdkpipelinets', 'master'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
